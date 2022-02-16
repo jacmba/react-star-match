77eaeacfa9f20596a21c23e36b5803653e64733f
Jacinto Mba Cantero <jacmba@gmail.com>
2022-02-16 15:31:11 +0000

    Game reset

--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { PlayAgain } from "./PlayAgain";
+import { PlayNumber } from "./PlayNumber";
+import { StarsDisplay } from "./StarsDisplay";
 
 // STAR MATCH - Starting Template
 
 const StarMatch = () => {
   const [stars, setStars] = useState(utils.random(1, 9));
+  const [availableNums, setAvailableNums] = useState(utils.range(1, 9))
+  const [candidateNums, setCandidateNums] = useState([])
+
+  const candidatesAreWrong = utils.sum(candidateNums) > stars;
+  const gameIsDone = availableNums.length === 0;
+
+  const numberStatus = number => {
+    if(!availableNums.includes(number)) {
+      return 'used';
+    }
+
+    if(candidateNums.includes(number)) {
+      return candidatesAreWrong ? 'wrong' : 'candidate';
+    }
+
+    return 'available';
+  };
+
+  const onNumberClick = (number, currentStatus) => {
+    if(currentStatus === 'used') {
+      return;
+    }
+
+    const newCandidateNums = currentStatus === 'available'
+      ? candidateNums.concat(number)
+      : candidateNums.filter(n => n !== number);
+
+    if (utils.sum(newCandidateNums) !== stars) {
+      setCandidateNums(newCandidateNums)
+    } else {
+      const newAvailableNums = availableNums.filter(n => !newCandidateNums.includes(n));
+      setStars(utils.randomSumIn(newAvailableNums, 9));
+      setAvailableNums(newAvailableNums);
+      setCandidateNums([]);
+    }
+  };
+
+  const resetGame = () => {
+    setStars(utils.random(1, 9));
+    setAvailableNums(utils.range(1,9));
+    setCandidateNums([])
+  }
+
   return (
     <div className="game">
       <div className="help">
@@ -13,27 +59,21 @@ const StarMatch = () => {
       </div>
       <div className="body">
         <div className="left">
-          {utils.range(1, stars).map(starId =>
-            <div key={starId} className="star" />
-          )}
+          {
+            gameIsDone
+            ? <PlayAgain onClick={resetGame} />
+            : <StarsDisplay range={utils.range(1, stars)} />
+          }
         </div>
         <div className="right">
           {utils.range(1, 9).map(n => 
-            <button key={n} className="number">{n}</button>
+            <PlayNumber key={n} number={n} status={numberStatus(n)} onClick={onNumberClick} />
           )}
         </div>
       </div>
       <div className="timer">Time Remaining: 10</div>
     </div>
   );
-};
-
-// Color Theme
-const colors = {
-  available: 'lightgray',
-  used: 'lightgreen',
-  wrong: 'lightcoral',
-  candidate: 'deepskyblue',
 };
 
 // Math science
