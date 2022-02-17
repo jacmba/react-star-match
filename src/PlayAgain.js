12776861ae39f6db209866b6d8e2d8f43d729189
Jacinto Mba <jcantero@fexco.com>
2022-02-17 10:43:28 +0000

    Side effect hooks

--- src/PlayAgain.js
+++ src/PlayAgain.js
@@ -1,5 +1,8 @@
-export const PlayAgain = props => (
+export const PlayAgain = (props) => (
   <div className="game-done">
+    <div className={`message ${props.gameStatus}`}>
+      {props.gameStatus === "lost" ? "Game Over" : "Winner!"}
+    </div>
     <button onClick={props.onClick}>Play Again</button>
   </div>
-)
+);
